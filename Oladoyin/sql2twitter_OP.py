ac88aba8123cd886661dc8917ed7b84a08076594
LadoKube <oladoyinphillips@gmail.com>
2016-09-16 15:51:50 +0100

    removed unnecessary imports

--- Oladoyin/sql2twitter_OP.py
+++ Oladoyin/sql2twitter_OP.py
@@ -1,30 +1,24 @@
-""" This script
-"""
+""" This script gets the table with the twitter data from sql, and loads it into a format that is readable by pythin
+by using json to convert each line into a dictionary)"""
 
-from twython import TwythonStreamer
+
 import pyodbc
 import json
-from collections import defaultdict
-
-from pandas import DataFrame, Series
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 # DSN means data source name
 connection = pyodbc.connect('DSN=kubricksql')
 cursor = connection.cursor()
 
-cursor.execute('select Tweet from dbo.Tweeps')
+cursor.execute('select Tweet from dbo.Friday1609')
 
 rows = cursor.fetchall() # gets all the rows from the tweet column
 
 tweet_list =[]
 
+# This creates a list of dictionaries
 for r in rows:
-   tweet_list.append(json.loads(r[0]))
+    tweet_list.append(json.loads(r[0]))
 
 
-print tweet_list[0]
 
 
